397d674ca01a2ae30ff7d4bd05d71d1dd13b51b8
Casuke <kylellamas723@gmail.com>
2026-05-08 10:41:46 +0800

    Create pipeline.lua

---
+++ pipeline.lua
@@ -0,0 +1,39 @@
+-- ============================================================
+-- NovaCrypt Obfuscator
+-- pipeline.lua
+-- ============================================================
+local Parser   = require("parser")
+local Unparser = require("unparser")
+
+local Pipeline = {}
+
+function Pipeline:new()
+    local p = {
+        parser   = Parser:new(),
+        unparser = Unparser:new(),
+        steps    = {},
+    }
+    setmetatable(p, self); self.__index = self
+    return p
+end
+
+function Pipeline:addStep(step)
+    table.insert(self.steps, step)
+end
+
+function Pipeline:apply(code)
+    math.randomseed(os.time())
+
+    
+    local ast = self.parser:parse(code)
+
+    
+    for _, step in ipairs(self.steps) do
+        step:apply(ast, self)
+    end
+
+    
+    return self.unparser:unparse(ast)
+end
+
+return Pipeline
